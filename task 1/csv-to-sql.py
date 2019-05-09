4471bbd91657ea7a1b48b28eee07c13b9820076e
onmax <maximogarciamtnez@gmail.com>
2019-05-09 21:34:25 +0200

    trying staff

---
+++ task 1/csv-to-sql.py
@@ -0,0 +1,13 @@
+import csv, sqlite3
+
+con = sqlite3.connect(":memory:")
+cur = con.cursor()
+cur.execute("CREATE TABLE stops (id, lat, lon, name);")
+
+with open('madrid-stops.csv','rt', encoding='utf-8') as fin:
+    dr = csv.DictReader(fin)
+    to_db = [(i['id'], i['lat'], i['lon'], i['name']) for i in dr]
+
+cur.executemany("INSERT INTO stops (id, lat, lon, name) VALUES (?, ?, ?, ?);", to_db)
+con.commit()
+con.close()
--- task 1/shell.sh
+++ task 1/shell.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 # Getting osmfilter
-sudo apt-get install osmctools
+sudo apt-get install osmctools python3-pip
 
 # Source data. Only needs to be done once
+# rm spain-latest.osm.pbf
 # wget http://download.geofabrik.de/europe/spain-latest.osm.pbf
 
 # For fast data processing, using o5m is recommended. Only needs to be done once
-# osmconvert spain-latest.osm.pbf -B=CiudadMadrid.poly.txt -o=spain.o5m
+osmconvert spain-latest.osm.pbf -B=CiudadMadrid.poly.txt -o=madrid.o5m
+
+rm madrid-stops.csv
+# Filter data
+osmfilter madrid.o5m  --keep-tags="building=train_station" -o=train-stations.o5m
 
 # Computer Science Vandal (CSV)
 # Filter only stops with name
-osmconvert spain.o5m --all-to-nodes --csv="@id @lat @lon name" --csv-headline | grep -i '[0-9]*[[:blank:]][0-9]*\.[0-9]*[[:blank:]]-[0-9]\.[0-9]*[[:blank:]][0-9a-zA-Z]' >> stops.csv
+# TODO: Improve sed pipe to allow full name. Rigth now, it splitting all name
+osmconvert train-stations.o5m --all-to-nodes --csv="@id @lat @lon name" --csv-headline | grep -i '[0-9]*[[:blank:]][0-9]*\.[0-9]*[[:blank:]]-[0-9]\.[0-9]*[[:blank:]][0-9a-zA-Z]' | sed 's/[[:digit:]][[:blank:]][[:digit:]]/,/g' >> madrid-stops.csv
+sed -i '1iid,lat,lon,name' madrid-stops.csv
+
+# python dependencies
+# pip3 install -r requirements.txt
+python3 csv-to-sql.py
